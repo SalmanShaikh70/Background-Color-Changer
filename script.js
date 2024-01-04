30de54942334a29709faedad4a137bdb2aac406e
Salman Shaikh <salmanshaikhsar123kar@gmail.com>
2024-01-05 01:56:49 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(params){
+        if(params.target.id === "grey"){
+            body.style.backgroundColor = params.target.id
+        }
+        if(params.target.id === "white"){
+            body.style.backgroundColor = params.target.id
+        }
+        if(params.target.id === "blue"){
+            body.style.backgroundColor = params.target.id
+        }
+        if(params.target.id === "yellow"){
+            body.style.backgroundColor = params.target.id
+        }
+        if(params.target.id === "orange"){
+            body.style.backgroundColor = params.target.id
+        }
+        if(params.target.id === "pink"){
+            body.style.backgroundColor = params.target.id
+        }
+    })
+})
